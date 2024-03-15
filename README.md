34ca3898e2bfe0c01d2e8923b2bcf701b04198b3
Mayra Zanchett Manchein <mayramanchein@yahoo.com.br>
2024-03-15 00:26:14 -0300

    add authentication in backend app

--- README.md
+++ README.md
@@ -13,7 +13,6 @@
    npm run dev
    ```
 
-
 ## Running the Frontend
 
 1. Navigate to the frontend directory:
@@ -47,7 +46,6 @@
    ```
 5. Repeat steps 2 and 3.
 
-
 #### For the backend, you need to set the following environment variables:
 
 - `PORT`: This is the port where your backend server will run.
@@ -56,17 +54,24 @@
 - `DB_HOST`: This is the host of your database.
 - `DB_PORT`: This is the port of your database.
 - `DB_NAME`: This is the name of your database.
+- `JWT_SECRET`: This is used to sign and verify JSON Web Tokens (JWTs) in your application
+  _Note: use the following code on server.js to generate a key for your JWT_SECRET, then add to .env file_
+
+  ```
+  const crypto = require('crypto');
+  const jwtSecret = crypto.randomBytes(32).toString('hex');
+  console.log(jwtSecret); // Save this secret key
+  ```
 
 #### For the frontend, you need to set the following environment variable:
 
-- `REACT_APP_API_URL`: Set this to `https://web-2xdz.onrender.com/api`
+- `REACT_APP_API_URL`: Set this to `https://web-2xdz.onrender.com/api` or `http://localhost:3001/api`
+- `REACT_APP_AUTHENTICATION_URL`: Set this to `https://web-2xdz.onrender.com` or `http://localhost:3001`
 
-*Note: The backend is deployed on the above service. If you want to test running locally, replace the URL with your backend localhost. It would probably be `http://localhost:3001/api`.*
-
+_Note: The backend is deployed on the above service. If you want to test running locally, replace the URL with your backend localhost. It would probably be `http://localhost:3001/api`._
 
 The frontend is also deployed for testing. You can access it at the following link: [https://web-mayrazan.vercel.app/](https://web-mayrazan.vercel.app/)
 
 You can consult my repository at [https://github.com/mayrazan/web](https://github.com/mayrazan/web).
-
 
 **To run the backend locally, you'll need to have PostgreSQL installed on your machine. The configuration for the database is provided in the section for creating an `.env` file.**
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+// const crypto = require('crypto');
 
 const knexfile = require('../knexfile.js');
 
@@ -33,10 +36,58 @@ async function recreateMoviesTable() {
 
 recreateMoviesTable();
 
+async function recreateUsersTable() {
+  const tableExists = await knexInstance.schema.hasTable('users');
+
+  if (tableExists) {
+    // drop table if it exists
+    await knexInstance.schema.dropTable('users');
+    console.log('Users table dropped!');
+  }
+
+  // create table
+  await knexInstance.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('username').unique().notNullable();
+    table.string('password').notNullable();
+    table.timestamps(true, true);
+  });
+  console.log('Users table created!');
+}
+
+recreateUsersTable();
+
+// Uncomment the following line to generate a new secret key and add to .env file
+// const jwtSecret = crypto.randomBytes(32).toString('hex');
+// console.log(jwtSecret); // Save this secret key
+
+// Middleware
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token === null) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
+    // Check if the token has expired
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+    if (user.exp < currentTimestamp) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    req.user = user;
+    next();
+  });
+}
+
 // Routes
 
 // Get all movies
-app.get('/api/movies', async (req, res) => {
+app.get('/api/movies', authenticateToken, async (req, res) => {
   try {
     const result = await knexInstance.select('*').from('movies');
     res.status(200).json(result);
@@ -47,7 +98,7 @@ app.get('/api/movies', async (req, res) => {
 });
 
 // Get movie by id
-app.get('/api/movies/:id', async (req, res) => {
+app.get('/api/movies/:id', authenticateToken, async (req, res) => {
   const { id } = req.params;
 
   try {
@@ -63,7 +114,7 @@ app.get('/api/movies/:id', async (req, res) => {
 });
 
 // Add movie
-app.post('/api/movies', async (req, res) => {
+app.post('/api/movies', authenticateToken, async (req, res) => {
   const { title, genre, year } = req.body;
   const requiredFields = { title, genre, year };
 
@@ -90,7 +141,7 @@ app.post('/api/movies', async (req, res) => {
 });
 
 // Delete movie
-app.delete('/api/movies/:id', async (req, res) => {
+app.delete('/api/movies/:id', authenticateToken, async (req, res) => {
   const { id } = req.params;
 
   try {
@@ -106,6 +157,84 @@ app.delete('/api/movies/:id', async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// register user
+app.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await knexInstance('users').insert({ username, password: hashedPassword });
+    res.status(201).send('User created');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+// login user
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await knexInstance('users').where({ username }).first();
+    if (user) {
+      const validPassword = await bcrypt.compare(password, user.password);
+      if (!validPassword) return res.status(401).send('Invalid password');
+
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+      res.json({ token });
+    } else {
+      res.status(404).send('User not found');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+// Get all users
+app.get('/users', authenticateToken, async (req, res) => {
+  try {
+    const users = await knexInstance('users').select('*');
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+// Get user by id
+app.get('/users/:id', authenticateToken, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await knexInstance('users').where({ id }).first();
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).send('User not found');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+// Delete user
+app.delete('/users/:id', authenticateToken, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await knexInstance('users').where({ id }).first();
+    if (user) {
+      await knexInstance('users').where({ id }).del();
+      res.send('User deleted successfully');
+    } else {
+      res.status(404).send('User not found');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
   }
 });
 
